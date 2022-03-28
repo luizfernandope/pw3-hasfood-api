eef1a3c3604cea74b0eddd58df0a33dd01aac7fd
Luiz-Fernando-Pegorari <luizevanda12345@gmail.com>
2022-03-28 07:56:15 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/luiz/fernando/pegorari/hasfood/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luiz.fernando.pegorari.hasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
